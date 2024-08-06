cf1b4b06a1015d4cbca287ee2914f2a3e396d8c2
justlikesh <lpsac@naver.com>
2024-08-06 20:09:28 +0900

    Feat : Jdbc Repository 구현 이렇게 JDBC API로 직접 코딩하는 것은 20년 전 이야기이다. 따라서 고대 개발자들이 이렇게 고생하고 살았구나 생각하고 참고하는 느낌으로 코딩해보자

--- src/main/java/hello8/_hello_spring/service/SpringConfig.java
+++ src/main/java/hello8/_hello_spring/service/SpringConfig.java
@@ -1,12 +1,23 @@
 package hello8._hello_spring.service;
 
+import hello8._hello_spring.repository.JdbcMemberRepository;
 import hello8._hello_spring.repository.MemberRepository;
 import hello8._hello_spring.repository.MemoryMemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+
+    private DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource){
+        this.dataSource = dataSource;
+    }
 
     @Bean     // 스프링 빈을 등록할꺼야
     public MemberService memberService(){
@@ -15,6 +26,7 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository(){
-        return new MemoryMemberRepository();
+//        return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 }
