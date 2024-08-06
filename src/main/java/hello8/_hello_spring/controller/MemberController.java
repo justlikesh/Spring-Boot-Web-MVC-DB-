dc45a9dc5976231a77bf06dca38353de6458fa4c
justlikesh <lpsac@naver.com>
2024-08-06 16:10:01 +0900

    Feat : 회원가입 기능 구현 (@PostMapping)

--- src/main/java/hello8/_hello_spring/controller/MemberController.java
+++ src/main/java/hello8/_hello_spring/controller/MemberController.java
@@ -1,8 +1,11 @@
 package hello8._hello_spring.controller;
 
+import hello8._hello_spring.domain.Member;
 import hello8._hello_spring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class MemberController {
@@ -12,5 +15,22 @@ public class MemberController {
     @Autowired
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
+    }
+
+    @GetMapping("/members/new")                   // 주로 조회할때 슨다
+    public String createForm(){
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new")             // 주로 데이터를 등록할때 쓴다
+    public String create(MemberForm form){
+        Member member = new Member();
+        member.setName(form.getName());
+
+        System.out.println("Member = " + member.getName());
+
+        memberService.join(member);
+
+        return "redirect:/";     // 홈화면으로
     }
 }
---
+++ src/main/java/hello8/_hello_spring/controller/MemberForm.java
@@ -0,0 +1,11 @@
+package hello8._hello_spring.controller;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class MemberForm {
+    private String name;
+}
